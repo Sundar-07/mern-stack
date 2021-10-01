af56e01e8c869dc77fb32e4c26811ba15e86c0dc
Sundararajan <43498008+Sundar-07@users.noreply.github.com>
2021-10-01 20:38:05 +0530

    tailwind css added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'tailwindcss/'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
