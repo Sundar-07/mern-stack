1078a6847c637b7c8444b5cffcfb4b3d9783f8ff
Sundararajan <43498008+Sundar-07@users.noreply.github.com>
2021-10-06 13:17:14 +0530

    added tailwindcss

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,28 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const NavBar = () => {
-    return ( 
-        <>
-        <h2>NavBar</h2>
-        <div class="grid grid-flow-col grid-rows-2 grid-cols-3 gap-4">
-  <div>
-    1
-  </div>
-  <div class="col-start-3">
-    2
-  </div>
-  <div>
-    3
-  </div>
-  <div>
-    4
-  </div>
-  <div class="row-start-1 col-start-2 col-span-2">
-    5
-  </div>
-</div>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <h2>NavBar</h2>
+      
+    </>
+  );
+};
+
 export default NavBar;
